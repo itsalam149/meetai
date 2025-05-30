99659997ecd4e3a0e1e63922e1fd5682edd22cc4
itsalam149 <alam014916@gmail.com>
2025-05-30 18:56:28 +0530

    03: authentication setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,74 @@
+"use client";
+
+import React from 'react';
+// import { createClient } from 'better-auth/react';
+import { authClient } from "@/lib/auth-client"; //import the auth client
+
+import { useState } from 'react';
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
+  const { data: session } = authClient.useSession() 
+
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = () => {
+    authClient.signUp.email({
+      email,
+      name,
+      password,
+    }, {
+      onError: () => {
+        window.alert("Something went wrong");
+      },
+      onSuccess: () => {
+        window.alert("Success");
+      }
+    });
+  }
+
+  const onLogin = () => {
+    authClient.signIn.email({
+      email,
+      password,
+    }, {
+      onError: () => {
+        window.alert("Something went wrong");
+      },
+      onSuccess: () => {
+        window.alert("Success");
+      }
+    });
+  }
+
+  if(session) {
+    return (
+      <div className="p-4 flex-col gap-4">
+        <h1 className="text-2xl">Welcome, {session.user.name}</h1>
+        <p className="text-sm">Email: {session.user.email}</p>
+        <Button onClick={() => authClient.signOut()}>Sign Out</Button>
+      </div>
+    );
+  }
+
   return (
-    <div className="text-4xl font-bold text-green-500">
-      Hello
+    <div className="flex flex-col gap-y-10">
+      <div className="p-4 flex-col gap-4">
+      <Input placeholder="name" value={name} onChange={(e) => setName(e.target.value)} />
+      <Input placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <Input placeholder="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <Button onClick={onSubmit}>Create User</Button>
     </div>
+    <div className="p-4 flex-col gap-4">
+      <Input placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <Input placeholder="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <Button onClick={onLogin}>Login</Button>
+    </div>
+    </div>
+    
   );
 }
